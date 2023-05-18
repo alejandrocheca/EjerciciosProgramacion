c40c4acefc2e863e146511589fe9a41924ac3d11
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2023-05-18 11:29:19 +0200

    Adivinar numero
    Adivinar numero aleatorio del 1 al 100 sin limite de intentos

---
+++ Java/AdivinarNumero/src/AdivinarNumero.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class AdivinarNumero {
+    public static void main(String[] args) {
+        int numeroSecreto = 42;
+        int intentos = 0;
+        int intento;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Adivina el número secreto (entre 1 y 100):");
+
+        while (true) {
+            intento = scanner.nextInt();
+            intentos++;
+
+            if (intento == numeroSecreto) {
+                System.out.println("¡Felicidades! Adivinaste el número en " + intentos + " intentos.");
+                break;
+            } else if (intento < numeroSecreto) {
+                System.out.println("El número secreto es mayor. Intenta nuevamente:");
+            } else {
+                System.out.println("El número secreto es menor. Intenta nuevamente:");
+            }
+        }
+
+        scanner.close();
+    }
+}

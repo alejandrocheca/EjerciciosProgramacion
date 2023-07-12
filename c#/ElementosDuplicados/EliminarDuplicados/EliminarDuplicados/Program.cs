f08901b0e500911b2d1ba5f319bc0e781932bfa9
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2023-07-12 15:34:20 +0200

    Eliminar letras duplicadas
    insertar un texto y recorrer todo el texto quitando las letras que se repitan de forma seguida

---
+++ c#/ElementosDuplicados/EliminarDuplicados/EliminarDuplicados/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace EliminarDuplicados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa un texto:");
+            string texto = Console.ReadLine();
+
+            string textoSinLetrasRepetidas = EliminarLetrasRepetidas(texto);
+
+            Console.WriteLine("Texto sin letras repetidas:");
+            Console.WriteLine(textoSinLetrasRepetidas);
+
+            Console.ReadLine();
+        }
+
+        static string EliminarLetrasRepetidas(string texto)
+        {
+            StringBuilder sb = new StringBuilder();
+            char letraAnterior = '\0'; // Carácter nulo para la primera letra
+
+            foreach (char letra in texto)
+            {
+                if (letra != letraAnterior)
+                {
+                    sb.Append(letra);
+                }
+                letraAnterior = letra;
+            }
+
+            return sb.ToString();
+        }
+    }
+}

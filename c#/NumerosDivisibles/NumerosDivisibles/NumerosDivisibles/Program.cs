dbf7d9e1f70d4c72610a33f96434a5bbc090c7d9
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2023-09-23 20:27:25 +0200

    Creacion ejercicio numeros divisibles
    decir del 1 al numero que quiera, y que me muestre por pantalla todos esos numeros que se puedan dividir por el numero que quiera

---
+++ c#/NumerosDivisibles/NumerosDivisibles/NumerosDivisibles/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa el número máximo del rango: ");
+        int maximo = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingresa el número por el cual deseas verificar la divisibilidad: ");
+        int divisor = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Números divisibles por {divisor} en el rango de 1 a {maximo}:");
+
+        for (int i = 1; i <= maximo; i++)
+        {
+            if (i % divisor == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}

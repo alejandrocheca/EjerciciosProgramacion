921a0c37ff9b31086771952627b07a43c3799659
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2023-09-28 11:20:53 +0200

    Creacion texto del reves
    ejercicio texto del reves en python

---
+++ Phyton/TextoDelReves/TextoDelReves.py
@@ -0,0 +1,8 @@
+# Solicitar al usuario que ingrese un texto
+texto = input("Introduce un texto: ")
+
+# Invertir el texto
+texto_al_reves = texto[::-1]
+
+# Mostrar el texto invertido
+print("Texto al revés:", texto_al_reves)

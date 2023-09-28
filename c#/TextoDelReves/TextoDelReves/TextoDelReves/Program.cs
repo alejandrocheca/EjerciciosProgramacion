35badd60009c16ef00dc192441e40fbd8cf8260b
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2023-09-28 10:58:20 +0200

    Creacion TextoDelReves
    Ejercicio donde le pasas un texto y lo muestra al reves

---
+++ c#/TextoDelReves/TextoDelReves/TextoDelReves/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Introduce un texto: ");
+        string texto = Console.ReadLine();
+
+        string textoAlReves = ReversoTexto(texto);
+
+        Console.WriteLine("Texto al revés: ");
+        Console.WriteLine(textoAlReves);
+    }
+
+    static string ReversoTexto(string texto)
+    {
+        char[] caracteres = texto.ToCharArray();
+        Array.Reverse(caracteres);
+        return new string(caracteres);
+    }
+}

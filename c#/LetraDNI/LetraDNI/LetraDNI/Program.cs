66b9618f3f807031fda9db8bf91a8486de41d3f7
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2023-10-22 13:35:31 +0200

    Buscar letra del DNI
    Buscar letra del DNI en C#

---
+++ c#/LetraDNI/LetraDNI/LetraDNI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduce el número de DNI (sin la letra): ");
+        string numeroDNI = Console.ReadLine();
+
+        if (int.TryParse(numeroDNI, out int numero))
+        {
+            char letra = CalcularLetraDNI(numero);
+            Console.WriteLine($"La letra correspondiente al número de DNI {numero} es: {letra}");
+        }
+        else
+        {
+            Console.WriteLine("El número de DNI no es válido. Debe ser un número entero.");
+        }
+    }
+
+    static char CalcularLetraDNI(int numero)
+    {
+        string letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+        int indice = numero % 23;
+        return letras[indice];
+    }
+}

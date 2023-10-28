f0891a6e1e51ef1ced8cb2c00f4e29eb114048d5
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2023-10-28 19:13:44 +0200

    Ejercicio texto binario
    Ejercicio texto binario en c#

---
+++ c#/TextoBinario/TextoBinario/TextoBinario/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduce un texto: ");
+        string texto = Console.ReadLine();
+
+        byte[] bytes = Encoding.UTF8.GetBytes(texto);
+
+        Console.WriteLine("Texto en binario:");
+        foreach (byte b in bytes)
+        {
+            Console.Write(Convert.ToString(b, 2).PadLeft(8, '0') + " ");
+        }
+    }
+}

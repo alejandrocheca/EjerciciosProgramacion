e47ff744bfc30037986cab8769f3cebeb331f72c
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2023-11-01 10:14:04 +0100

    Ejercicio terminal adivinando numero
    Ejercicio terminal adivinando numero en VB

---
+++ VB/TerminalAdivinandoNumero/TerminalAdivinandoNumero/TerminalAdivinandoNumero/Program.vb
@@ -0,0 +1,29 @@
+Imports System
+
+Module Program
+    Sub Main()
+        Dim numeroObjetivo As Integer = 42
+        Dim intentos As Integer = AdivinarNumero(numeroObjetivo)
+        Console.WriteLine("Se adivinó el número " & numeroObjetivo & " en " & intentos & " intentos.")
+    End Sub
+
+    Function AdivinarNumero(numeroObjetivo As Integer) As Integer
+        Dim intentos As Integer = 0
+        Dim numeroAdivinado As Integer
+
+        Do
+            ' Generar un número aleatorio para adivinar
+            Dim rnd As New Random()
+            numeroAdivinado = rnd.Next(1, 101) ' Suponemos que el número está entre 1 y 100
+
+            ' Incrementar el contador de intentos
+            intentos += 1
+
+            ' Mostrar el número adivinado
+            Console.WriteLine("Intento " & intentos & ": " & numeroAdivinado)
+
+        Loop While numeroAdivinado <> numeroObjetivo
+
+        Return intentos
+    End Function
+End Module

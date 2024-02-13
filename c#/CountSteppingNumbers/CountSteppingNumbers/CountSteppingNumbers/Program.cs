ec62814a3de8d9052b96bf403e90bb8331695f11
alejandrocheca <67010108+alejandrocheca@users.noreply.github.com>
2024-02-13 19:35:02 +0100

    Ejercicio CountSteppingNumbers
    Ejercicio CountSteppingNumbers en C#

---
+++ c#/CountSteppingNumbers/CountSteppingNumbers/CountSteppingNumbers/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int CountSteppingNumbers(string low, string high)
+    {
+        List<int> result = new List<int>();
+        int lowNum = int.Parse(low);
+        int highNum = int.Parse(high);
+
+        void DFS(int num)
+        {
+            if (lowNum <= num && num <= highNum)
+            {
+                result.Add(num);
+            }
+
+            if (num == 0 || num > highNum)
+            {
+                return;
+            }
+
+            int lastDigit = num % 10;
+            if (lastDigit > 0)
+            {
+                DFS(num * 10 + lastDigit - 1);
+            }
+            if (lastDigit < 9)
+            {
+                DFS(num * 10 + lastDigit + 1);
+            }
+        }
+
+        for (int i = 0; i <= 9; i++)
+        {
+            DFS(i);
+        }
+
+        return result.Count;
+    }
+
+    public static void Main()
+    {
+        Solution solution = new Solution();
+
+        // Example usage:
+        string low1 = "1", high1 = "11";
+        Console.WriteLine(solution.CountSteppingNumbers(low1, high1));  // Output: 10
+
+        string low2 = "90", high2 = "101";
+        Console.WriteLine(solution.CountSteppingNumbers(low2, high2));  // Output: 2
+    }
+}
